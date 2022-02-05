3e6e302052af80f2fd42ec81072a03702b1e9aa1
aboba123123 <denonishenko@gmail.com>
2022-02-05 17:16:10 +0300

    work.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xvrop075#+vat$dka%lx%r3ti+yj12ext5-_s74jl(9$4&4x7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users'
 ]
 
 MIDDLEWARE = [
